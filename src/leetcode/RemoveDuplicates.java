11bef700507fe7abf1f28264196023252a81855a
xueye <xueye@renrenche.com>
2019-02-19 11:21:38 +0800

    有效的括号 + 删除排序数组中的重复项

---
+++ src/leetcode/RemoveDuplicates.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * 删除排序数组中的重复项
+ *
+ * https://leetcode-cn.com/explore/interview/card/tencent/221/array-and-strings/902/
+ *
+ */
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{}));
+    }
+    public static int removeDuplicates(int[] nums) {
+        int a = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[a]) {
+                a++;
+                nums[a] = nums[i];
+            }
+        }
+        return a + 1;
+    }
+
+
+}
---
+++ src/leetcode/ValidParentheses.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * 有效的括号
+ *
+ * https://leetcode-cn.com/explore/interview/card/tencent/221/array-and-strings/901/
+ *
+ */
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        System.out.println(isValid(""));
+    }
+    public static boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Stack stack = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' ||  s.charAt(i) == '{') {
+                stack.push(s.charAt(i));
+            }
+            if (s.charAt(i) == ')' || s.charAt(i) == ']' ||  s.charAt(i) == '}') {
+                char p;
+                try {
+                     p = (char)stack.pop();
+                } catch (Exception e) {
+                    return false;
+                }
+                if ((s.charAt(i) == ')' && p =='(')
+                        || (s.charAt(i) == ']' && p =='[')
+                        || (s.charAt(i) == '}' && p =='{')) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (stack.empty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
