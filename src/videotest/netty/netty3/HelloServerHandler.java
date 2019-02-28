46c197c44dadd5c07472e4d8bf722d3677c0c7a8
xueye <xueye@renrenche.com>
2019-02-28 10:31:56 +0800

    netty5的客户端和服务端

--- src/videotest/netty/netty3/HelloServerHandler.java
+++ src/videotest/netty/netty3/HelloServerHandler.java
@@ -1,8 +1,10 @@
-package videotest.netty;
+package videotest.netty.netty3;
 
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.*;
 
-public class HelloClientHandler extends SimpleChannelHandler{
+public class HelloServerHandler extends SimpleChannelHandler{
 
     @Override
     public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
@@ -13,7 +15,21 @@ public class HelloClientHandler extends SimpleChannelHandler{
     @Override
     public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
         System.out.println("messageReceived");
-        super.messageReceived(ctx, e);
+        // 接受数据
+        /*
+        ChannelBuffer message = (ChannelBuffer)e.getMessage();
+        String result = new String(message.array());
+        System.out.println(result);
+         */
+        System.out.println(e.getMessage());
+
+        // 回写数据
+        /*(需要用ChannelBuffer包起来)
+        ChannelBuffer channelBuffer = ChannelBuffers.copiedBuffer("hi".getBytes());
+        ctx.getChannel().write(channelBuffer);
+        */
+//        ctx.getChannel().write("bbb");
+//        super.messageReceived(ctx, e);
     }
 
     @Override
@@ -23,24 +39,12 @@ public class HelloClientHandler extends SimpleChannelHandler{
     }
 
 
-    /**
-     * 必须是链接已经建立，关闭通道的时候才会触发
-     * @param ctx
-     * @param e
-     * @throws Exception
-     */
     @Override
     public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
         System.out.println("channelDisconnected");
         super.channelDisconnected(ctx, e);
     }
 
-    /**
-     * channel关闭的时候触发
-     * @param ctx
-     * @param e
-     * @throws Exception
-     */
     @Override
     public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
         System.out.println("channelClosed");
---
+++ src/videotest/netty/netty5/Server.java
@@ -0,0 +1,69 @@
+package videotest.netty.netty5;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class Server {
+
+    public static void main(String[] args) {
+        // 服务类
+        ServerBootstrap bootstrap = new ServerBootstrap();
+
+        // EventLoopGroup 对boss worker进行封装
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup work = new NioEventLoopGroup();
+
+        // 设置线程池
+        bootstrap.group(boss, work);
+
+        // 设置socket工厂
+        bootstrap.channel(NioServerSocketChannel.class);
+
+        // 设置管道工厂
+        bootstrap.childHandler(new ChannelInitializer<Channel>() {
+
+            @Override
+            protected void initChannel(Channel channel) throws Exception {
+                channel.pipeline().addLast(new StringDecoder());
+                channel.pipeline().addLast(new StringEncoder());
+                channel.pipeline().addLast(new ServerHandler());
+            }
+
+        });
+
+        // 设置参数
+        // 对serverSocketchannel的设置，TCP主机有一个队列保存链接，accept会获取一个channel，会从队列取出一个链接，最多能链接2048，连接缓冲池
+        bootstrap.option(ChannelOption.SO_BACKLOG, 2048);
+        // 对socketChannel的设置，维持连接的活跃，清除死链接
+        bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
+        // 对socketChannel的设置，关闭延迟发送
+        bootstrap.option(ChannelOption.TCP_NODELAY, true);
+
+
+        // 绑定端口
+        ChannelFuture bind = bootstrap.bind(10101);
+
+        System.out.println("start");
+
+        // 等待服务端关闭
+        try {
+            // 这样会阻塞在这里，等待channel运行完再关闭
+            bind.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // 优雅的关闭
+            boss.shutdownGracefully();
+            work.shutdownGracefully();
+        }
+
+    }
+
+
+
+}
