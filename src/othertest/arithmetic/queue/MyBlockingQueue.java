bef1d3d9a006e184852624c0b22865c431b482a3
xueye01 <xueye01@58.com>
2020-11-16 20:54:22 +0800

    排序

---
+++ src/othertest/arithmetic/queue/MyBlockingQueue.java
@@ -0,0 +1,131 @@
+package othertest.arithmetic.queue;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 阻塞队列
+ * @param <E>
+ */
+public class MyBlockingQueue<E> {
+    final ReentrantLock lock ;
+    final Condition notFull;
+    final Condition notEmpty;
+    private int takeIndex = 0;
+    private int putIndex = 0;
+    //计数器
+    private int count = 0;
+    //容量
+    private final int capacity;
+    private final Object[] items;
+    //默认按照公平锁实现简易版
+    public MyBlockingQueue(int capacity) {
+        this.capacity = capacity;
+        items = new Object[capacity];
+        lock = new ReentrantLock();
+        notFull = lock.newCondition();
+        notEmpty = lock.newCondition();
+    }
+
+    public void put(E e) throws InterruptedException {
+        //响应中断
+        lock.lockInterruptibly();
+        try {
+            //元素个数等于数组长度
+            while (items.length == count) {
+                notFull.await();
+            }
+            items[putIndex] = e;
+            ++putIndex;
+            System.out.println("生产者线程:" + count + ",putIndex=" + putIndex);
+            if (putIndex == items.length) {
+                putIndex = 0;
+            }
+            count++;
+            notEmpty.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public E take() throws InterruptedException {
+        lock.lockInterruptibly();
+        try {
+            //元素个数为0
+            while (count == 0) {
+                notEmpty.await();
+            }
+            E e = (E)items[takeIndex];
+            items[takeIndex] = null;
+            System.out.println("消费者线程count：" + count + ",takeIndex=" + takeIndex);
+            //刚开始写成了++takeIndex == count
+            if (++takeIndex == items.length) {
+                System.out.println("居然为0");
+                takeIndex = 0;
+            }
+
+            count--;
+            notFull.signal();
+            return e;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MyBlockingQueue<String> queue = new MyBlockingQueue<>(10);
+
+        Thread producer1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i=0;
+                while (true) {
+                    try {
+                        queue.put((i++) + "");
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+       /* Thread producer2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        queue.put(Math.random() + "");
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });*/
+
+        Thread consumer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        String ele = queue.take();
+                        System.out.println("消费者线程获取元素" + ele);
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        producer1.start();
+        // producer2.start();
+        consumer.start();
+
+        producer1.join();
+        //producer2.join();
+        consumer.join();
+    }
+}
+
---
+++ src/othertest/arithmetic/recursion/Recursion.java
@@ -0,0 +1,37 @@
+package othertest.arithmetic.recursion;
+
+/**
+ * 递归
+ */
+public class Recursion {
+
+
+    /**
+     * 假如这里有 n 个台阶，每次你可以跨 1 个台阶或者 2 个台阶，请问走这 n 个台阶有多少种走法？
+     * 可以根据第一步的走法把所有走法分为两类，第一类是第一步走了 1 个台阶，另一类是第一步走了 2 个台阶。所以 n 个台阶的走法就等于先走 1 阶后，n-1 个台阶的走法 加上先走 2 阶后，n-2 个台阶的走法
+     * 因为递归本身就是借助栈来实现的，只不过我们使用的栈是系统或者虚拟机本身提供的，我们没有感知罢了
+     *
+     * @param n
+     * @return
+     */
+    int f(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        return f(n-1) + f(n-2);
+    }
+
+    int f2(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int ret = 0;
+        int pre = 2;
+        int prepre = 1;
+        for (int i = 3; i <= n; ++i) {
+            ret = pre + prepre;
+            prepre = pre;
+            pre = ret;
+        }
+        return ret;
+    }
+
+}
